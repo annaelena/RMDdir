f34f70674931e86f362caad8ccc62eb5c9e4ec49
carlo spadotto <carlospadotto.sanso@gmail.com>
2016-05-16 14:28:42 +0200

    gigi dag

---
+++ RMDIR/app/src/main/java/com/app/rmdir/rmdir/ListFragment.java
@@ -0,0 +1,32 @@
+package com.app.rmdir.rmdir;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListFragment extends Fragment {
+
+
+    public ListFragment() {
+        // Required empty public constructor
+    }
+    public static ListFragment getIstance()
+    {
+        return new ListFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+}
